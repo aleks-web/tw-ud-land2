d4bc0a4ec984b9ae2c4ca5b29b19d06b4e095ac9
Alex <dok.go@yandex.ru>
2026-06-03 15:05:57 +0500

    fix

--- src/js/events.js
+++ src/js/events.js
@@ -2,5 +2,9 @@ import { Fancybox } from "@fancyapps/ui";
 
 document.addEventListener('fetchLeadSuccess', (e) => {
     Fancybox.close();
-    window.location.href = '/success.html';
+    ym(97164229,'reachGoal','send_form')
+
+    setTimeout(() => {
+        window.location.href = '/success.html';
+    }, 200);
 });
