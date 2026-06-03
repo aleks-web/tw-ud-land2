0c1435a3ef3a03aee3373dc3fce12089d8a79351
Alex <dok.go@yandex.ru>
2026-06-03 15:02:16 +0500

    fix

--- src/js/events.js
+++ src/js/events.js
@@ -2,6 +2,5 @@ import { Fancybox } from "@fancyapps/ui";
 
 document.addEventListener('fetchLeadSuccess', (e) => {
     Fancybox.close();
-    // openModal("#modal-success");
     window.location.href = '/success.html';
 });
